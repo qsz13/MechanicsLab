b34d57af0ccc99306f58aa4def418defea78d320
dyslove123 <7675343.die@163.com>
2015-06-11 16:42:44 +0800

    animation
    dys

--- DisplayBoard/MainWindow.xaml.cs
+++ DisplayBoard/MainWindow.xaml.cs
@@ -13,25 +13,48 @@ using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+
 
 namespace DisplayBoard
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
+        
         System.Windows.Threading.DispatcherTimer Timer = new System.Windows.Threading.DispatcherTimer();
- 
+        int elpase = 0;
+        string temp = "";
+        DoubleAnimation dax = new DoubleAnimation();
+        DoubleAnimation day = new DoubleAnimation();
+
         public MainWindow()
         {
             InitializeComponent();
             initTimer();
             initClock();
             initDate();
-            this.button1.Click+= new RoutedEventHandler(button_click);
+            initAnimation();
         }
+        private void initAnimation()
+        {
 
+            //指定起点  
+            dax= new DoubleAnimation();
+            day = new DoubleAnimation();
+            dax.From = 400;
+            day.From = 0;
+            //指定终点  
+            dax.To = 0;
+            day.To = 0;
+            //指定时长  
+            Duration duration = new Duration(TimeSpan.FromMilliseconds(3000));
+            dax.Duration = duration;
+            day.Duration = duration;
+        }
         private void initTimer()
         {
             Timer.Tick += new EventHandler(Timer_Click);
@@ -43,7 +66,10 @@ namespace DisplayBoard
         public void initClock()
         {
             DateTime d = DateTime.Now;
-            clock.Text = d.Hour + " : " + d.Minute;
+            if(d.Minute<10)
+                clock.Text = d.Hour + " : 0" + d.Minute;
+            else
+                clock.Text = d.Hour + " : " + d.Minute;
             
         }
 
@@ -51,45 +77,75 @@ namespace DisplayBoard
         {
             DateTime d = DateTime.Now;
             int[] arr = { d.Year, d.Month, d.Day };
-
-            date_cn.Text = string.Join(".", arr);
+            date_cn.Text = string.Join(".", arr) + " " + d.DayOfWeek+ "今日实验 ："+"???";
+            
         }
-
+        private void change_content(Grid g,MyMessage m)
+        {
+            m.getmm();
+            for(int i=0;i<5;i++){
+                Viewbox vb = g.Children[i] as Viewbox;
+                TextBlock tb = vb.Child as TextBlock;
+                tb.Text = m.mm[i];
+            }
+        }
+        private class MyMessage 
+        {
+            public MyMessage(String s){
+                m_statu = m_statu + s;
+            }
+            public String m_lab = "1号实验室";
+            public String m_statu = "进行:";
+            public String m_content = "未知名实验";
+            public String m_class = "神秘地带";
+            public String m_people = "黑衣人";
+            public String[] mm=new String[8];
+            public void getmm()
+            {
+                mm[0] = m_lab;
+                mm[1] = m_statu;
+                mm[2] = m_content;
+                mm[3] = m_class;
+                mm[4] = m_people;
+            }
+        }
         private void Timer_Click(object sender, EventArgs e)
         {
-            DateTime d = DateTime.Now;
-            clock.Text = d.Hour + " : " + d.Minute;
+            initDate();
+            initClock();
+            elpase++;
+            if (elpase == 2)
+            {
+                //input you message
+                change_content(this.view1, new MyMessage(temp));
+                change_content(this.view3, new MyMessage(temp));
+                change_content(this.view4, new MyMessage(temp));
+            }
 
-        }
-
-        private void ListView_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-        private void button_click(object sender,RoutedEventArgs e)
-        {
-            MessageBox.Show("222233333");
-            DoubleAnimation []dax = new DoubleAnimation[5];
-            DoubleAnimation []day = new DoubleAnimation[5];
-            //指定起点  
-            dax[0] = new DoubleAnimation();
-            day[0]=new DoubleAnimation();
-            dax[0].From = 300;
-            day[0].From = 0;
-
-            //指定终点  
-            dax[0].To = 0;
-            day[0].To = 0;
+            if (elpase > 4)
+            {
+                temp = clock.Text+DateTime.Now.Second;
+                elpase = 0;
+                Start_animation();
+            }
             
-            //指定时长  
-            Duration duration = new Duration(TimeSpan.FromMilliseconds(3000));
-            dax[0].Duration = duration;
-            day[0].Duration = duration;
 
-            //动画主体是TranslatTransform变形，而非Button  
-            this.tt3_copy.BeginAnimation(TranslateTransform.XProperty, dax[0]);
-            this.tt3_copy.BeginAnimation(TranslateTransform.YProperty, day[0]);  
-        
+        }
+
+        private void Start_animation()
+        {
+            //input your message
+            change_content(this.view1_Copy, new MyMessage(temp));
+            change_content(this.view3_Copy, new MyMessage(temp));
+            change_content(this.view4_Copy, new MyMessage(temp));
+
+            this.tt1_copy.BeginAnimation(TranslateTransform.XProperty, dax);
+            this.tt1_copy.BeginAnimation(TranslateTransform.YProperty, day); 
+            this.tt3_copy.BeginAnimation(TranslateTransform.XProperty, dax);
+            this.tt3_copy.BeginAnimation(TranslateTransform.YProperty, day);
+            this.tt4_copy.BeginAnimation(TranslateTransform.XProperty, dax);
+            this.tt4_copy.BeginAnimation(TranslateTransform.YProperty, day);
+
         }
 
         
