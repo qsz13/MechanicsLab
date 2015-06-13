20d0703db583bf2950316b9944971c37517cef46
dyslove123 <7675343.die@163.com>
2015-06-13 14:14:04 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	QuerySystem/Model/MyMessage.cs

--- QuerySystem/Model/MyMessage.cs
+++ QuerySystem/Model/MyMessage.cs
@@ -9,37 +9,62 @@ namespace QuerySystem.Model
     class MyMessage
     {
         public MyMessage(){ }
-        public String recode_id = "0";
-        public String date = "1999-08-01";
-        public String time = "12节课程";
-        public String duty = "";
-        public String m_lab = "1号实验室";
+        private String recode_id = "0";
+
+        public String Recode_id
+        {
+            get { return recode_id; }
+            set { recode_id = value; }
+        }
+        private String date = "1999-08-01";
+
+        public String Date
+        {
+            get { return date; }
+            set { date = value; }
+        }
+        private String time = "1,2节课程";
+
+        public String Time
+        {
+            get { return time; }
+            set { time = value; }
+        }
+
+        private String m_lab = "本部115";
+
+        public String Lab
+        {
+            get { return m_lab; }
+            set { m_lab = value; }
+        }
+        private String m_content = "拉伸与收缩实验";
+
+        public String Content
+        {
+            get { return m_content; }
+            set { m_content = value; }
+        }
+        private String pass_statu = "已通过";
+
+        public String Pass_statu
+        {
+            get { return pass_statu; }
+            set { pass_statu = value; }
+        }
 
         public String m_statu = "时段2- 正在进行";
-        public String m_content = "实验内容";
+        
         public String m_class = "1100201 材料力学 蒋建华";
         public String m_people = "老师名字";
         public String[] mm = new String[8];
         public void getmm()
         {
-            mm[0] = m_lab;
+            mm[0] = Lab;
             mm[1] = m_statu;
-            mm[2] = m_content;
+            mm[2] = Content;
             mm[3] = m_class;
             mm[4] = m_people;
-        }
-        public override bool Equals(object obj)
-        {
-
-            MyMessage a = obj as MyMessage;
-            a.getmm();
-            this.getmm();
-            for (int i = 0; i < 5; i++)
-            {
-                if (!mm[0].Equals(a.mm[0]))
-                    return false;
-            }
-            return true;
         }
 
     }
